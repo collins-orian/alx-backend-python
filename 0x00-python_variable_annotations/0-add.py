7fba972a59393f66972554e8d6450f2c322e00fd
collins-orian <inaboyacollins@icloud.com>
2023-07-07 12:25:21 +0100

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
+'''this file contains an add function'''
+
+
 def add(a: float, b: float) -> float:
+    '''the function takes in two parameters of float type
+    and returns a float type value'''
+
     return a+b
