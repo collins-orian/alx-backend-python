ce8d5d232cc054b4f50e26f949c3b6e07cf4bd8c
collins-orian <inaboyacollins@icloud.com>
2023-07-07 12:28:39 +0100

    Create 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+'''this file contains the concat function'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''the function takes in two parameters of string type
+    and returns a concatenation of both strings'''
+
+    return str1+str2
