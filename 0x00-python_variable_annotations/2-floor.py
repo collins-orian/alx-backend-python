6c84f66c39ed15044fdb66eae6dbcfbe5601e880
collins-orian <inaboyacollins@icloud.com>
2023-07-07 13:11:22 +0100

    Create 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+'''this file contains the floor function'''
+
+
+def floor(n: float) -> int:
+    '''the function takes in a parameter of float type
+    and returns a floor of that value'''
+
+    return int(n//1)
