81c58f83b7cc1d193aef67c9de6341bb63267834
collins-orian <inaboyacollins@icloud.com>
2023-07-07 14:10:45 +0100

    Create 4-define_variables.py

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+'''this file contains some defined variables'''
+
+# a, an integer with a value of 1
+a: int = 1
+
+# pi, a float with a value of 3.14
+pi: float = 3.14
+
+# i_understand_annotations, a boolean with a value of True
+i_understand_annotations: bool = True
+
+# school, a string with a value of “Holberton”
+school: str = "Holberton"
