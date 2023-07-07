f410a105004a1390ed4ea4feb9b0b38a9cef9ad9
collins-orian <inaboyacollins@icloud.com>
2023-07-07 14:04:20 +0100

    Create 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+'''this file contains the to_str function'''
+
+
+def to_str(n: float) -> str:
+    '''the function takes in a parameters of float type
+    and returns a string representation of the parameters'''
+
+    return str(n)
