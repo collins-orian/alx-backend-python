696dd77710c07f2e18bad3ed604d3e4575070ccc
collins-orian <inaboyacollins@icloud.com>
2023-07-07 22:01:22 +0100

    Create 9-element_length.py

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,12 @@
+'''Annotate the below function’s parameters and
+return values with the appropriate types
+    def element_length(lst):
+        return [(i, len(i)) for i in lst]
+'''
+
+from typing import List, Tuple, Iterable, Sequence
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''this function returns the number of elements '''
+    return [(i, len(i)) for i in lst]
