1d646656e4f1e536bce29fc7acd97fd8aa439865
collins-orian <inaboyacollins@icloud.com>
2023-07-07 22:16:28 +0100

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -14,4 +14,4 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple(str, float):
     '''Converts a key and its value to a tuple of the key and
     the square of its value.
     '''
-    return (k, v**2)
+    return (k, float(v**2))
