3d84176e5f0ff6d1477bbabbac3e7f5f2c5aba33
collins-orian <inaboyacollins@icloud.com>
2023-07-14 17:02:29 +0100

    updates to tasks 2 and 3

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -14,10 +14,8 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-
-    for i in range(n):
-        start: float = time.time()
-        asyncio.run(wait_n(n, max_delay))
-        end: float = time.time()
-        total_time: float = end - start
-    return total_time / n
+    '''Computes the average runtime of wait_n.
+    '''
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    return (time.time() - start_time) / n
--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,12 +6,13 @@
 to do this) task_wait_random that takes an integer max_delay and
 returns a asyncio.Task.'''
 
+
 import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> None:
-    """This function returns a task"""
-
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    '''Creates an asynchronous task for wait_random.
+    '''
     return asyncio.create_task(wait_random(max_delay))
