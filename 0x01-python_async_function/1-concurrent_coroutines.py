be4c47ac66ae51885dbc7dcb9dbd325eec8b1313
collins-orian <inaboyacollins@icloud.com>
2023-07-14 15:53:20 +0100

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -21,3 +21,16 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
         delay = await wait_random(max_delay)
         delays.append(delay)
     return sorted(delays)
+
+"""Another way of handling this task is to use the asyncio.gather() within
+the asyncio module function as follows:
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''Executes wait_random n times.
+    '''
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
+"""
