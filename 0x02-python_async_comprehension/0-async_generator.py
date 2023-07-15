a9e61110d8b3917a7853ecedc64daa9dae685f97
collins-orian <inaboyacollins@icloud.com>
2023-07-16 00:48:06 +0100

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,10 +9,10 @@ Use the random module.
 
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     '''Generates a sequence of 10 numbers.
     '''
     for _ in range(10):
